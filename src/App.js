568e401dd78e377589298d5179157c9b6476d22d
marimeli <marimel.casas@gmail.com>
2018-10-29 07:41:32 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
+import Navigation from './components/Navigation';
+
 
 class App extends Component {
 
@@ -15,30 +18,44 @@ class App extends Component {
       .then(res => res.json())
       .then(data => {
         this.setState({
-          artists: data.artists.artist,
+          artists: data.artists.artist.map((artist, i) => {
+            return {
+              name: artist.name,
+              image: artist.image[3]['#text']
+            }
+          })
         })
       })
   };
 
   render() {
-    console.log('la data 37', this.state.artists);
+    console.log('la data 29', this.state.artists);
 
     return (
+
       <div className="App" >
-        {this.state.artists.length ?
-          this.state.artists.map((artist, i) =>
-            // console.log('objArtist', artist);
-
-            // console.log(
-            //   artist.name,
-            //   artist.image[3]['#text']
-            // )
-
-            <div key={i}>
-              Name: {artist.name} | Imagen: <img src={artist.image[3]['#text']}/>
-            </div>)
-          : <div> Cargando ...</div>
-        }
+        <Navigation></Navigation>
+        <Container className="p-5">
+          <Row>
+            <Col xs="12">
+              {this.state.artists.length ?
+                this.state.artists.map((artist, i) =>
+                  <Row>
+                    <Col xs="12" key={i} className="pt-3">
+                      {artist.name}
+                      <i class="fas fa-heart ml-2"></i>
+                      <i class="fas fa-thumbs-down ml-2"></i>
+                    </Col>
+                    <Col xs="12" key={i} className="pt-3">
+                      <i class="fas fa-angle-left"></i><img src={artist.image} alt="" /><i class="fas fa-angle-right"></i>
+                    </Col>
+                  </Row>
+                )
+                : <p> Cargando ...</p>
+              }
+            </Col>
+          </Row>
+        </Container>
       </div>
 
     );
---
+++ src/components/Navigation.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar= () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Navbar color="faded" light>
+          <NavbarBrand href="/" className="mr-auto">Ranking Songs</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/components/">Artists</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Tracks</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
