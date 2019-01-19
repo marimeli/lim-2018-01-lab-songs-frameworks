b4726a504c045bf1b341493588e6d96dbf619e1a
marimeli <marimel.casas@gmail.com>
2019-01-19 02:07:22 -0500

    Updates

---
+++ precache-manifest.8d6ed57554248b07620a9117db33e3fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45d975ffd74dd0f719d3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/runtime~main.45d975ff.js"
+  },
+  {
+    "revision": "335402930c7f425ce366",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/main.33540293.chunk.js"
+  },
+  {
+    "revision": "58fe247220ff83febc25",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/1.58fe2472.chunk.js"
+  },
+  {
+    "revision": "335402930c7f425ce366",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/css/main.df69112c.chunk.css"
+  },
+  {
+    "revision": "58fe247220ff83febc25",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/css/1.891d443e.chunk.css"
+  },
+  {
+    "revision": "be7581129c3b2887c1c714a0977b886b",
+    "url": "/lim-2018-01-lab-songs-frameworks/index.html"
+  }
+];
